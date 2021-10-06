147cffbe0ce9fbea7cf417574f24249ad4d88005
MyaFryckman22 <817258@mystma.org>
2021-10-06 14:31:01 -0500

    3?

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
+#define function 
+def factorial(x):
+     total = 1
+     for i in range (1, x+1):
+        total = total * i
+     return total
+
+print(factorial(5))
+
+#should return 120 if run correctly, please run correctly
